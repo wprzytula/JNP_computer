adcde1e6a119d5d511f2345c46955492c817c164
wprzytula <wp418383@students.mimuw.edu.pl>
2020-12-10 23:00:20 +0100

    some asserts

--- computer.h
+++ computer.h
@@ -6,6 +6,7 @@
 #include <cassert>
 #include <type_traits>
 
+
 using num_id_t = unsigned long long;
 
 template <size_t n, typename T>
@@ -13,6 +14,263 @@ using memory_t = std::array<T, n>;
 
 template <size_t n>
 using vars_t = std::array<num_id_t, n>;
+
+
+// TMPAsm Elements
+
+enum instruction_t {JUMP, DECLARATION, LABEL, INSTRUCTION};
+enum element_t {NUM, LEA, MEM};
+
+
+constexpr auto Id(const char* id) {
+    num_id_t num_id = 0ULL;
+    unsigned i = 0;
+    assert(id[0] != '\0' && ((id[0] >= '0' && id[0] <= '9') ||
+           (id[0] >= 'A' && id[0] <= 'Z') || (id[0] >= 'a' && id[0] <= 'z')));
+    while (id[i] != '\0') {
+        num_id += ((num_id_t)(unsigned char)(id[i] >= 'a' ? id[i] - ('a' - 'A') : id[i]))
+                << (8U * i);
+        ++i;
+        assert(i < 7);
+    }
+    return num_id;
+}
+
+template <auto num>
+struct Num {
+    static constexpr element_t el_type = NUM;
+    template <size_t n, typename T>
+    static constexpr auto rval(const vars_t<n>&, const memory_t <n, T>&) {
+        return T(num);
+    }
+};
+
+template <typename Addr>
+struct Mem {
+    static constexpr element_t el_type = MEM;
+    template <size_t n, typename T>
+    static constexpr auto* addr(vars_t<n> vars, memory_t <n, T>& memory) {
+        return &memory[Addr::template rval<n, T>(vars, memory)];
+    }
+    template <size_t n, typename T>
+    static constexpr auto const& rval(vars_t<n> vars, const memory_t<n, T>& memory) {
+        return memory[Addr::template rval<n, T>(vars, memory)];
+    }
+};
+
+template <num_id_t num_id>
+struct Lea {
+    static constexpr element_t el_type = LEA;
+    template<size_t n, typename T>
+    static constexpr size_t rval(const vars_t<n>& vars, const memory_t<n, T>&) {
+        for (size_t i = 0; i < n; ++i) {
+            if (vars[i] == num_id)
+                return i;
+        }
+        assert(false);
+    }
+};
+
+
+// TMPAsm Instructions
+
+struct Instruction {};
+
+template <num_id_t id, typename Value>
+struct D : Instruction {
+    static_assert(Value::el_type == NUM);
+    static constexpr instruction_t ins_type = DECLARATION;
+    template <size_t n, typename T>
+    static constexpr void declare(vars_t<n>& vars, memory_t<n, T>& memory) {
+        size_t i = 0;
+        while (i < n) {
+            if (vars[i] == 0) {
+                vars[i] = id;
+                memory[i] = Value::rval(vars, memory);
+                return;
+            }
+            ++i;
+        }
+        assert(false);
+    }
+};
+
+template <typename LValue, typename RValue>
+struct Mov : Instruction {
+    static constexpr instruction_t ins_type = INSTRUCTION;
+    template <size_t n, typename T>
+    static constexpr void execute(vars_t<n>& vars, memory_t<n, T>& memory, bool&, bool&) {
+        *LValue::template addr<n, T>(vars, memory) =
+                RValue::template rval<n, T>(vars, memory);
+    }
+};
+
+template <typename LValue, typename RValue>
+struct Add : Instruction {
+    static constexpr instruction_t ins_type = INSTRUCTION;
+    template <size_t n, typename T>
+    static constexpr void execute(vars_t<n>& vars, memory_t <n, T>& memory, bool& ZF, bool& SF) {
+        T result = (T)(*LValue::template addr<n, T>(vars, memory) +=
+                RValue::template rval<n, T>(vars, memory));
+        ZF = result == (T)0;
+        SF = result < (T)0;
+    }
+};
+
+template <typename LValue, typename RValue>
+struct Sub : Instruction {
+    static constexpr instruction_t ins_type = INSTRUCTION;
+    template <size_t n, typename T>
+    static constexpr void execute(vars_t<n> vars, memory_t<n, T>& memory, bool& ZF, bool& SF) {
+        auto* lval = LValue::template addr<n, T>(vars, memory);
+        auto rval = RValue::template rval<n, T>(vars, memory);
+        T result = *lval = (T)(*lval - rval);
+        ZF = result == (T)0;
+        SF = result < (T)0;
+    }
+};
+
+template <typename LValue>
+using Inc = Add<LValue, Num<1>>;
+
+template <typename LValue>
+using Dec = Sub<LValue, Num<1>>;
+
+
+// miejsce na Adamowe Cmp i operacje bitowe
+
+
+template <num_id_t id>
+struct Label : Instruction {
+    static constexpr instruction_t ins_type = LABEL;
+    static constexpr num_id_t label = id;
+};
+
+template <num_id_t L>
+struct Jmp : Instruction {
+    static constexpr instruction_t ins_type = JUMP;
+    static constexpr num_id_t label = L;
+    template <size_t n, typename T>
+    static constexpr bool should_jump(const bool&, const bool&) {
+        return true;
+    }
+};
+
+template <num_id_t L>
+struct Js : Instruction {
+    static constexpr instruction_t ins_type = JUMP;
+    static constexpr num_id_t label = L;
+    template <size_t n, typename T>
+    static constexpr bool should_jump(const bool&, const bool& SF) {
+        return SF;
+    }
+};
+
+template <num_id_t L>
+struct Jz : Instruction {
+    static constexpr instruction_t ins_type = JUMP;
+    static constexpr num_id_t label = L;
+    template <size_t n, typename T>
+    static constexpr bool should_jump(const bool& ZF, const bool&) {
+        return ZF;
+    }
+};
+
+
+/*template <size_t n, typename T, typename T2>
+struct isDeclaration {
+    static constexpr bool result = false;
+};
+
+template<size_t n, typename T>
+using execute_t = void (vars_t<n>&, memory_t <n, T>&, bool&, bool&);
+
+template<size_t n, typename T>
+using declare_t = void (vars_t<n>&, memory_t <n, T>&);
+
+template <size_t n, typename T>
+struct isDeclaration <n, T, execute_t<n, T>> {
+static constexpr bool result = false;
+};
+
+template <size_t n, typename T>
+struct isDeclaration <n, T, declare_t<n, T>> {
+    static constexpr bool result = true;
+};*/
+
+
+template <typename...>
+struct Program;
+
+template <>
+struct Program <> {
+    template <size_t n, typename T>
+    static constexpr void declare(vars_t<n>&, memory_t<n, T>&) {}
+    template <size_t n, typename T, typename P>
+    static constexpr void run(vars_t<n>&, memory_t<n, T>&,
+                              bool&, bool&) {}
+    template <size_t n, typename T, typename P>
+    static constexpr void jump(vars_t<n>&, memory_t<n, T>&,
+                               bool&, bool&) {
+        assert(false);
+    }
+};
+
+template <typename Line, typename ...rest>
+struct Program <Line, rest...> {
+    static_assert(std::is_base_of<Instruction, Line>::value);
+
+    template <size_t n, typename T>
+    static constexpr void declare(vars_t<n>& vars, memory_t<n, T>& memory) {
+        if constexpr (Line::ins_type == DECLARATION)
+            Line::template declare<n, T>(vars, memory);
+        Program<rest...>::template declare<n, T>(vars, memory);
+    }
+
+    template <size_t n, typename T, typename P>
+    static constexpr void run(vars_t<n>& vars,
+            memory_t<n, T>& memory, bool& ZF, bool& SF) {
+        if constexpr (Line::ins_type == JUMP) {
+            if (Line::template should_jump<n, T>(ZF, SF))
+                P::template jump<n, T, P, Line::label>(vars, memory, ZF, SF);
+            else
+                Program<rest...>::template run<n, T, P>(vars, memory, ZF, SF);
+        }
+        else {
+            if constexpr (Line::ins_type == INSTRUCTION)
+                Line::template execute<n, T>(vars, memory, ZF, SF);
+            Program<rest...>::template run<n, T, P>(vars, memory, ZF, SF);
+        }
+    }
+
+    template <size_t n, typename T, typename P, num_id_t label>
+    static constexpr void jump(vars_t<n>& vars, memory_t<n, T>& memory,
+                               bool& ZF, bool& SF) {
+        if constexpr (Line::ins_type == LABEL) {
+            if constexpr (Line::label == label)
+                run<n, T, P>(vars, memory, ZF, SF);
+            else
+                Program<rest...>::template jump<n, T, P, label>(vars, memory, ZF, SF);
+        } else {
+            Program<rest...>::template jump<n, T, P, label>(vars, memory, ZF, SF);
+        }
+
+    }
+};
+
+template <size_t n, typename T>
+struct Computer {
+    template <typename P>
+    static constexpr memory_t<n, T> boot() {
+        memory_t<n, T> memory {0};
+        vars_t<n> vars {0};
+        bool ZF = false;
+        bool SF = false;
+        P::template declare<n, T>(vars, memory);
+        P::template run<n, T, P>(vars, memory, ZF, SF);
+        return memory;
+    }
+};
 
 
 /*
@@ -69,258 +327,5 @@ struct ct_map2<> {};
 template<num_id_t k, size_t v, typename... rest>
 struct ct_map2<kv<k, v>, rest...> {};
 */
-
-
-
-// TMPAsm Elements
-
-enum instruction_t {JUMP, DECLARATION, LABEL, INSTRUCTION};
-
-constexpr num_id_t Id(const char* id) {
-    num_id_t num_id = 0ULL;
-    unsigned i = 0;
-    assert(id[0] != '\0' && ((id[0] >= '0' && id[0] <= '9') ||
-           (id[0] >= 'A' && id[0] <= 'Z') || (id[0] >= 'a' && id[0] <= 'z')));
-    while (id[i] != '\0') {
-        num_id += ((num_id_t)(unsigned char)(id[i] >= 'a' ? id[i] - ('a' - 'A') : id[i]))
-                << (8U * i);
-        ++i;
-        assert(i < 7);
-    }
-    return num_id;
-}
-
-template <auto num>
-struct Num {
-    template <size_t n, typename T>
-    static constexpr auto rval(const vars_t<n>&, const memory_t <n, T>&) {
-//        static_assert(std::is_integral<decltype(num)>::value); -> to zbędne, bo n jako parametr
-//                                                                    array załatwia sprawę
-        return num;
-    }
-};
-
-template <typename Addr>
-struct Mem {
-    template <size_t n, typename T>
-    static constexpr auto* addr(vars_t<n> vars, std::array<T, n>& memory) {
-        bool result = Addr::template rval<n, T>(vars, memory);
-        assert(result < n);
-        return &memory[Addr::template rval<n, T>(vars, memory)];
-    }
-    template <size_t n, typename T>
-    static constexpr auto const& rval(vars_t<n> vars, const memory_t<n, T>& memory) {
-        return memory[Addr::template rval<n, T>(vars, memory)];
-    }
-};
-
-template <num_id_t num_id>
-struct Lea {
-    template<size_t n, typename T>
-    static constexpr size_t rval(const vars_t<n>& vars, const memory_t<n, T>&) {
-        for (size_t i = 0; i < n; ++i) {
-            if (vars[i] == num_id)
-                return i;
-        }
-//        return 0;
-        assert(false);
-    }
-};
-
-
-// TMPAsm Instructions
-
-template <num_id_t id, typename Value>
-struct D {
-    static constexpr instruction_t type = DECLARATION;
-    template <size_t n, typename T>
-    static constexpr void declare(vars_t<n>& vars, memory_t<n, T>& memory) {
-        size_t i = 0;
-        while (i < n) {
-            if (vars[i] == 0) {
-                vars[i] = id;
-                memory[i] = Value::rval(vars, memory);
-                return;
-            }
-            ++i;
-        }
-        assert(false);
-    }
-};
-
-template <typename LValue, typename RValue>
-struct Mov {
-    static constexpr instruction_t type = INSTRUCTION;
-    template <size_t n, typename T>
-    static constexpr void execute(vars_t<n>& vars, memory_t<n, T>& memory, bool&, bool&) {
-        *LValue::template addr<n, T>(vars, memory) =
-                RValue::template rval<n, T>(vars, memory);
-    }
-};
-
-template <typename LValue, typename RValue>
-struct Add {
-    static constexpr instruction_t type = INSTRUCTION;
-    template <size_t n, typename T>
-    static constexpr void execute(vars_t<n>& vars, memory_t <n, T>& memory, bool& ZF, bool& SF) {
-        auto result = *LValue::template addr<n, T>(vars, memory) +=
-                RValue::template rval<n, T>(vars, memory);
-        ZF = result == 0;
-        SF = result < 0;
-    }
-};
-
-template <typename LValue, typename RValue>
-struct Sub {
-    static constexpr instruction_t type = INSTRUCTION;
-    template <size_t n, typename T>
-    static constexpr void execute(vars_t<n> vars, memory_t<n, T>& memory, bool& ZF, bool& SF) {
-        auto result = *LValue::template addr<n, T>(vars, memory) -=
-                RValue::template rval<n, T>(vars, memory);
-        ZF = result == 0;
-        SF = result < 0;
-    }
-};
-
-template <typename LValue>
-using Inc = Add<LValue, Num<1>>;
-
-template <typename LValue>
-using Dec = Sub<LValue, Num<1>>;
-
-
-// miejsce na Adamowe Cmp i operacje bitowe
-
-
-template <num_id_t id>
-struct Label {
-    static constexpr instruction_t type = LABEL;
-    static constexpr num_id_t label = id;
-};
-
-template <num_id_t L>
-struct Jmp {
-    static constexpr instruction_t type = JUMP;
-    static constexpr num_id_t label = L;
-    template <size_t n, typename T>
-    static constexpr bool should_jump(const bool&, const bool&) {
-        return true;
-    }
-};
-
-template <num_id_t L>
-struct Js {
-    static constexpr instruction_t type = JUMP;
-    static constexpr num_id_t label = L;
-    template <size_t n, typename T>
-    static constexpr bool should_jump(const bool&, const bool& SF) {
-        return SF;
-    }
-};
-
-template <num_id_t L>
-struct Jz {
-    static constexpr instruction_t type = JUMP;
-    static constexpr num_id_t label = L;
-    template <size_t n, typename T>
-    static constexpr bool should_jump(const bool& ZF, const bool&) {
-        return ZF;
-    }
-};
-
-
-/*template <size_t n, typename T, typename T2>
-struct isDeclaration {
-    static constexpr bool result = false;
-};
-
-template<size_t n, typename T>
-using execute_t = void (vars_t<n>&, memory_t <n, T>&, bool&, bool&);
-
-template<size_t n, typename T>
-using declare_t = void (vars_t<n>&, memory_t <n, T>&);
-
-template <size_t n, typename T>
-struct isDeclaration <n, T, execute_t<n, T>> {
-static constexpr bool result = false;
-};
-
-template <size_t n, typename T>
-struct isDeclaration <n, T, declare_t<n, T>> {
-    static constexpr bool result = true;
-};*/
-
-
-template <typename...>
-struct Program;
-
-template <>
-struct Program <> {
-    template <size_t n, typename T>
-    static constexpr void declare(vars_t<n>&, memory_t<n, T>&) {}
-    template <size_t n, typename T, typename P>
-    static constexpr void run(vars_t<n>&,
-                              memory_t<n, T>&, bool&, bool&) {}
-    template <size_t n, typename T, typename P>
-    static constexpr void jump(vars_t<n>&, memory_t<n, T>&,
-                               bool&, bool&) {
-        assert(false);
-    }
-};
-
-template <typename Line, typename ...rest>
-struct Program <Line, rest...> {
-
-    template <size_t n, typename T>
-    static constexpr void declare(vars_t<n>& vars, memory_t<n, T>& memory) {
-        if constexpr (Line::type == DECLARATION)
-            Line::template declare<n, T>(vars, memory);
-        Program<rest...>::template declare<n, T>(vars, memory);
-    }
-
-    template <size_t n, typename T, typename P>
-    static constexpr void run(vars_t<n>& vars,
-            memory_t<n, T>& memory, bool& ZF, bool& SF) {
-        if constexpr (Line::type == JUMP) {
-            if (Line::template should_jump<n, T>(ZF, SF))
-                P::template jump<n, T, P, Line::label>(vars, memory, ZF, SF);
-            else
-                Program<rest...>::template run<n, T, P>(vars, memory, ZF, SF);
-        }
-        else {
-            if constexpr (Line::type == INSTRUCTION)
-                Line::template execute<n, T>(vars, memory, ZF, SF);
-            Program<rest...>::template run<n, T, P>(vars, memory, ZF, SF);
-        }
-    }
-
-    template <size_t n, typename T, typename P, num_id_t label>
-    static constexpr void jump(vars_t<n>& vars, memory_t<n, T>& memory,
-                               bool& ZF, bool& SF) {
-        if constexpr (Line::type == LABEL) {
-            if constexpr (Line::label == label)
-                run<n, T, P>(vars, memory, ZF, SF);
-            else
-                Program<rest...>::template jump<n, T, P, label>(vars, memory, ZF, SF);
-        } else {
-            Program<rest...>::template jump<n, T, P, label>(vars, memory, ZF, SF);
-        }
-
-    }
-};
-
-template <size_t n, typename T>
-struct Computer {
-    template <typename P>
-    static constexpr memory_t<n, T> boot() {
-        memory_t<n, T> memory {0};
-        vars_t<n> vars {0};
-        bool ZF = false;
-        bool SF = false;
-        P::template declare<n, T>(vars, memory);
-        P::template run<n, T, P>(vars, memory, ZF, SF);
-        return memory;
-    }
-};
 
 #endif //INC_4_COMPUTER_H
---
+++ computer_tests.cc
@@ -0,0 +1,153 @@
+#include "computer.h"
+#include <array>
+#include <iostream>
+
+// Operator == dla std::array jest constexpr dopiero od C++20.
+template<class T, std::size_t N>
+constexpr bool compare(std::array<T, N> const& arg1, std::array<T, N> const& arg2) {
+    for (size_t i = 0; i < N; ++i)
+        if (arg1[i] != arg2[i]) return false;
+    return true;
+}
+
+/*
+ * Tests by Wojciech Przytuła
+ * All rights left
+ * */
+
+using test_machine = Computer<4, int>;
+
+// the following tests presume that you use unsigned long long for storing IDs
+
+
+// tests that should compile and not raise static_assert errors
+
+using test_empty = Program<>;
+constexpr std::array<int, 4> test_empty_res = {0, 0, 0, 0};
+static_assert(compare(test_machine::boot<test_empty>(), test_empty_res));
+
+using test_jumps = Program<
+        Dec<Mem<Num<0>>>,
+        Jz<Id("XD")>,
+        Inc<Mem<Num<0>>>,
+        Jz<Id("XD")>,
+        Inc<Mem<Num<1>>>,
+        Label<Id("xd")>
+>;
+constexpr std::array<int, 4> test_jumps_res = {0, 0, 0, 0};
+static_assert(compare(test_machine::boot<test_jumps>(), test_jumps_res));
+
+using test_declarations = Program<
+        D<Id("a"), Num<-3>>,
+        D<Id("b"), Num<-2>>,
+        D<Id("c"), Num<-1>>,
+        D<Id("a"), Num<-1>>
+>;
+constexpr std::array<int, 4> test_declarations_res = {-3, -2, -1, -1};
+static_assert(compare(test_machine::boot<test_declarations>(), test_declarations_res));
+
+
+// tests that should compile AND ofc not result in infinite loop:
+using test_id1 = Program<
+        D<Id("a"), Num<1>>,
+        Label<Id("a")>,
+        Dec<Mem<Num<0>>>,
+        Js<Id("end")>,
+        Label<Id("A")>,
+        Jz<Id("A")>,
+        Label<Id("loop")>,
+        Jmp<Id("loop")>,
+        Label<Id("end")>
+        >;
+
+using test_underflow = Program<
+        D<Id("mem0"), Num<INT32_MIN>>,
+        Dec<Mem<Lea<Id("MEM0")>>>>;
+//constexpr auto test_underflow_res = test_machine::boot<test_underflow>();
+
+using test_finite_loop = Program<
+        Label<Id("loop")>,
+        Dec<Mem<Lea<Id("MEM0")>>>,
+        Js<Id("LOOP")>,
+        D<Id("mem0"), Num<INT32_MIN + 100>>
+>;
+//constexpr auto test_finite_loop_res = test_machine::boot<test_finite_loop>();
+
+
+// tests that should not compile (i.e. template parsing error):
+
+//using test_bad_id1 = Program<
+//        Label<Id("%")>>;
+
+//using test_bad_id2 = Program<
+//        Label<Id("3324234")>>;
+
+//using test_bad_syntax1 = Program<
+//        Inc<Lea<Lea<Id("a")>>>>;
+
+
+/* tests that should not run as constexpr,
+ * i.e. constexpr res = machine::boot<test>() should result in compile-time error.
+ * (if they don't even compile then it's good either way) */
+
+using test_infinite_loop = Program<
+        Label<Id("loop")>,
+        Mov<Mem<Lea<Id("MEM0")>>, Num<0>>,
+        Jmp<Id("LOOP")>,
+        D<Id("mem0"), Num<-1000000>>
+>;
+//constexpr auto test_infinite_loop_res = test_machine::boot<test_infinite_loop>();
+
+using test_bad_program = Num<4>;
+//constexpr auto bad_program = test_machine::boot<test_bad_program>();
+
+using test_size1 = Program<
+        Mov<Mem<Num<2137>>, Num<1>>>;
+//constexpr auto test_size1_res = test_machine::boot<test_size1>();
+
+using test_syntax1 = Program<
+        Num<0>>;
+//constexpr auto test_syntax1_res = test_machine::boot<test_syntax1>();
+
+using test_syntax2 = Program<
+        Lea<Id("a")>>;
+//constexpr auto test_syntax2_res = test_machine::boot<test_syntax2>();
+
+using test_syntax3 = Program<
+        Mem<Num<0>>>;
+//constexpr auto test_syntax3_res = test_machine::boot<test_syntax3>();
+
+using test_syntax4 = Program<
+        Inc<Num<0>>>;
+//constexpr auto test_syntax4_res = test_machine::boot<test_syntax4>();
+
+using test_syntax5 = Program<
+        Inc<Inc<Num<0>>>>;
+//constexpr auto test_syntax5_res = test_machine::boot<test_syntax5>();
+
+// I can't make this test fail, yet I believe it should.
+using test_syntax6 = Program<
+        Label<4ULL>>;
+//constexpr auto test_syntax6_res = test_machine::boot<test_syntax6>();
+
+using test_D_syntax1 = Program<
+        D<Id("a"), Mem<Num<1>>>>;
+//constexpr auto test_D_syntax1_res = test_machine::boot<test_D_syntax1>();
+
+using test_D_syntax2 = Program<
+        D<Id("1"), Num<1>>,
+        D<Id("a"), Lea<Id("1")>>>;
+//constexpr auto test_D_syntax2_res = test_machine::boot<test_D_syntax1>();
+
+
+
+//int main() {
+//
+//
+//test_machine::boot<test_finite_loop>();
+//test_machine::boot<test_id1>();
+//
+//auto arr = test_machine::boot<test_id1>();
+//for (auto itr : arr)
+//    std::cout << (int) itr << std::endl;
+//}
