63f5acf917315b4319682b26e167310884f1f9c4
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-12-12 17:46:08 +0100

    Code refactor.

--- computer.h
+++ computer.h
@@ -19,22 +19,27 @@ using vars_t = std::array<num_id_t, n>;
 
 // TMPAsm Elements
 
-// [TODO]: Consider changing to enum class.
-enum instruction_t {JUMP, DECLARATION, LABEL, INSTRUCTION};
-enum element_t {NUM, LEA, MEM};
+enum class instruction_t {JUMP, DECLARATION, LABEL, INSTRUCTION};
+enum class element_t {NUM, LEA, MEM};
+
+constexpr bool is_id_sign_incorrect(char ch) {
+  return !((ch >= '0' && ch <= '9') ||
+      (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'));
+}
 
 constexpr num_id_t Id(const char* id) {
     num_id_t num_id = 0ULL;
-    unsigned i = 0;
-    if (id[0] == '\0' || ((id[0] <= '0' || id[0] >= '9')
-                            && (id[0] <= 'A' || id[0] >= 'Z')
-                            && (id[0] <= 'a' || id[0] >= 'z')))
+    size_t i = 0;
+
+    if (id[0] == '\0' || is_id_sign_incorrect(id[0]))
       throw std::exception();
+
     while (id[i] != '\0') {
         num_id += ((num_id_t) (unsigned char) (id[i] >= 'a' ? id[i] - ('a' - 'A') : id[i]))
                 << (8U * i);
         ++i;
-        if(i >= 7)
+
+        if (i >= 7)
           throw std::exception();
     }
     return num_id;
@@ -42,7 +47,8 @@ constexpr num_id_t Id(const char* id) {
 
 template <auto num>
 struct Num {
-    static constexpr element_t el_type = NUM;
+    static constexpr element_t el_type = element_t::NUM;
+
     template <size_t n, typename T>
     static constexpr auto rval(const vars_t<n>&, const memory_t <n, T>&) {
         return T(num);
@@ -51,11 +57,13 @@ struct Num {
 
 template <typename Addr>
 struct Mem {
-    static constexpr element_t el_type = MEM;
+    static constexpr element_t el_type = element_t::MEM;
+
     template <size_t n, typename T>
     static constexpr auto* addr(vars_t<n> vars, memory_t <n, T>& memory) {
         return &memory[Addr::template rval<n, T>(vars, memory)];
     }
+
     template <size_t n, typename T>
     static constexpr auto const& rval(vars_t<n> vars, const memory_t<n, T>& memory) {
         return memory[Addr::template rval<n, T>(vars, memory)];
@@ -64,7 +72,8 @@ struct Mem {
 
 template <num_id_t num_id>
 struct Lea {
-    static constexpr element_t el_type = LEA;
+    static constexpr element_t el_type = element_t::LEA;
+
     template<size_t n, typename T>
     static constexpr size_t rval(const vars_t<n>& vars, const memory_t<n, T>&) {
         for (size_t i = 0; i < n; ++i) {
@@ -81,8 +90,11 @@ struct Instruction {};
 
 template <num_id_t id, typename Value>
 struct D : Instruction {
-    static_assert(Value::el_type == NUM, "Value parameter of D instruction must be Num!");
-    static constexpr instruction_t ins_type = DECLARATION;
+    static_assert(Value::el_type == element_t::NUM,
+        "Value parameter of D instruction must be Num!");
+
+    static constexpr instruction_t ins_type = instruction_t::DECLARATION;
+
     template <size_t n, typename T>
     static constexpr void declare(vars_t<n>& vars, memory_t<n, T>& memory) {
         size_t i = 0;
@@ -100,7 +112,8 @@ struct D : Instruction {
 
 template <typename LValue, typename RValue>
 struct Mov : Instruction {
-    static constexpr instruction_t ins_type = INSTRUCTION;
+    static constexpr instruction_t ins_type = instruction_t::INSTRUCTION;
+
     template <size_t n, typename T>
     static constexpr void execute(vars_t<n>& vars, memory_t<n, T>& memory, bool&, bool&) {
         *LValue::template addr<n, T>(vars, memory) =
@@ -110,7 +123,8 @@ struct Mov : Instruction {
 
 template <typename LValue, typename RValue>
 struct Add : Instruction {
-    static constexpr instruction_t ins_type = INSTRUCTION;
+    static constexpr instruction_t ins_type = instruction_t::INSTRUCTION;
+
     template <size_t n, typename T>
     static constexpr void execute(vars_t<n>& vars, memory_t <n, T>& memory, bool& ZF, bool& SF) {
         T* lval = LValue::template addr<n, T>(vars, memory);
@@ -123,7 +137,8 @@ struct Add : Instruction {
 
 template <typename LValue, typename RValue>
 struct Sub : Instruction {
-    static constexpr instruction_t ins_type = INSTRUCTION;
+    static constexpr instruction_t ins_type = instruction_t::INSTRUCTION;
+
     template <size_t n, typename T>
     static constexpr void execute(vars_t<n> vars, memory_t<n, T>& memory, bool& ZF, bool& SF) {
         T* lval = LValue::template addr<n, T>(vars, memory);
@@ -142,7 +157,8 @@ using Dec = Sub<LValue, Num<1>>;
 
 template <typename RValue1, typename RValue2>
 struct Cmp : Instruction {
-  static constexpr instruction_t ins_type = INSTRUCTION;
+  static constexpr instruction_t ins_type = instruction_t::INSTRUCTION;
+
   template <size_t n, typename T>
   static constexpr void execute(vars_t<n> vars, memory_t<n, T>& memory, bool& ZF, bool& SF) {
     auto result = RValue1::template rval<n, T>(vars, memory) -
@@ -154,7 +170,8 @@ struct Cmp : Instruction {
 
 template <typename LValue, typename RValue>
 struct And : Instruction {
-    static constexpr instruction_t ins_type = INSTRUCTION;
+    static constexpr instruction_t ins_type = instruction_t::INSTRUCTION;
+
     template <size_t n, typename T>
     static constexpr void execute(vars_t<n>& vars, memory_t <n, T>& memory, bool& ZF, bool&) {
         auto result = (*LValue::template addr<n, T>(vars, memory) &=
@@ -165,7 +182,8 @@ struct And : Instruction {
 
 template <typename LValue, typename RValue>
 struct Or : Instruction {
-    static constexpr instruction_t ins_type = INSTRUCTION;
+    static constexpr instruction_t ins_type = instruction_t::INSTRUCTION;
+
     template <size_t n, typename T>
     static constexpr void execute(vars_t<n>& vars, memory_t <n, T>& memory, bool& ZF, bool&) {
         auto result = (*LValue::template addr<n, T>(vars, memory) |=
@@ -176,7 +194,8 @@ struct Or : Instruction {
 
 template <typename LValue>
 struct Not : Instruction {
-    static constexpr instruction_t ins_type = INSTRUCTION;
+    static constexpr instruction_t ins_type = instruction_t::INSTRUCTION;
+
     template <size_t n, typename T>
     static constexpr void execute(vars_t<n>& vars, memory_t <n, T>& memory, bool& ZF, bool&) {
         auto result = (*LValue::template addr<n, T>(vars, memory) =
@@ -187,14 +206,15 @@ struct Not : Instruction {
 
 template <num_id_t id>
 struct Label : Instruction {
-    static constexpr instruction_t ins_type = LABEL;
+    static constexpr instruction_t ins_type = instruction_t::LABEL;
     static constexpr num_id_t label = id;
 };
 
 template <num_id_t L>
 struct Jmp : Instruction {
-    static constexpr instruction_t ins_type = JUMP;
+    static constexpr instruction_t ins_type = instruction_t::JUMP;
     static constexpr num_id_t label = L;
+
     static constexpr bool should_jump(const bool&, const bool&) {
         return true;
     }
@@ -202,8 +222,9 @@ struct Jmp : Instruction {
 
 template <num_id_t L>
 struct Js : Instruction {
-    static constexpr instruction_t ins_type = JUMP;
+    static constexpr instruction_t ins_type = instruction_t::JUMP;
     static constexpr num_id_t label = L;
+
     static constexpr bool should_jump(const bool&, const bool& SF) {
         return SF;
     }
@@ -211,8 +232,9 @@ struct Js : Instruction {
 
 template <num_id_t L>
 struct Jz : Instruction {
-    static constexpr instruction_t ins_type = JUMP;
+    static constexpr instruction_t ins_type = instruction_t::JUMP;
     static constexpr num_id_t label = L;
+
     static constexpr bool should_jump(const bool& ZF, const bool&) {
         return ZF;
     }
@@ -225,9 +247,11 @@ template <>
 struct Program <> {
     template <size_t n, typename T>
     static constexpr void declare(vars_t<n>&, memory_t<n, T>&) {}
+
     template <size_t n, typename T, typename P>
     static constexpr void run(vars_t<n>&, memory_t<n, T>&,
                               bool&, bool&) {}
+
     template <size_t n, typename T, typename P>
     static constexpr void jump(vars_t<n>&, memory_t<n, T>&,
                                bool&, bool&) {
@@ -241,7 +265,7 @@ struct Program <Line, rest...> {
 
     template <size_t n, typename T>
     static constexpr void declare(vars_t<n>& vars, memory_t<n, T>& memory) {
-        if constexpr (Line::ins_type == DECLARATION)
+        if constexpr (Line::ins_type == instruction_t::DECLARATION)
             Line::template declare<n, T>(vars, memory);
         Program<rest...>::template declare<n, T>(vars, memory);
     }
@@ -249,15 +273,16 @@ struct Program <Line, rest...> {
     template <size_t n, typename T, typename P>
     static constexpr void run(vars_t<n>& vars,
             memory_t<n, T>& memory, bool& ZF, bool& SF) {
-        if constexpr (Line::ins_type == JUMP) {
+        if constexpr (Line::ins_type == instruction_t::JUMP) {
             if (Line::should_jump(ZF, SF))
                 P::template jump<n, T, P, Line::label>(vars, memory, ZF, SF);
             else
                 Program<rest...>::template run<n, T, P>(vars, memory, ZF, SF);
         }
         else {
-            if constexpr (Line::ins_type == INSTRUCTION)
+            if constexpr (Line::ins_type == instruction_t::INSTRUCTION)
                 Line::template execute<n, T>(vars, memory, ZF, SF);
+
             Program<rest...>::template run<n, T, P>(vars, memory, ZF, SF);
         }
     }
@@ -265,7 +290,7 @@ struct Program <Line, rest...> {
     template <size_t n, typename T, typename P, num_id_t label>
     static constexpr void jump(vars_t<n>& vars, memory_t<n, T>& memory,
                                bool& ZF, bool& SF) {
-        if constexpr (Line::ins_type == LABEL) {
+        if constexpr (Line::ins_type == instruction_t::LABEL) {
             if constexpr (Line::label == label)
                 run<n, T, P>(vars, memory, ZF, SF);
             else
@@ -283,8 +308,10 @@ struct Computer {
         vars_t<n> vars {0};
         bool ZF = false;
         bool SF = false;
+
         P::template declare<n, T>(vars, memory);
         P::template run<n, T, P>(vars, memory, ZF, SF);
+
         return memory;
     }
 };
